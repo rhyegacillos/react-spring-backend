4b16f6ff9e58580a9cc69a0acc5c2bd5cc851497
gacillos.rhye@gmail.com <3ch0s3r@>
2023-01-03 01:11:49 +0800

    initial commit

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { moviesReducer, addMovie, removeMovie} from "./slices/moviesSlice";
+import { songsReducer, addSong, removeSong } from "./slices/songsSlice";
+import {reset} from './actions'
+
+
+const store = configureStore({
+    reducer: {
+        songs: songsReducer,
+        movies: moviesReducer
+    }
+
+});
+
+export {store, addSong, removeSong, addMovie, removeMovie, reset}
+
